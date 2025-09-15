aaba47e8f13c4a4772d5720555b763c5852fcc8a
viniciusnascimento-me <vinicius.adao.nascimento@gmail.com>
2025-09-15 14:29:20 -0300

    updates

--- index.js
+++ index.js
@@ -6,11 +6,11 @@
 
 const cvtBtn = document.getElementById("cvt-btn");
 let uniInput = document.getElementById("uni-input");
-const lenghtDiv = document.getElementById("length-box");
+const lengthDiv = document.getElementById("length-box");
 const volumeDiv = document.getElementById("volume-box");
 const massDiv = document.getElementById("mass-box");
 
-cvtBtn.addEventListener("click", function calcConversion(numbConvert) {
+function calcConversion(numbConvert) {
   numbConvert = Number(uniInput.value);
 
   lengthImp = numbConvert * 3.28084; // pega o valor do INPUT e transforma de Metrico -> Imperial
@@ -22,20 +22,14 @@ cvtBtn.addEventListener("click", function calcConversion(numbConvert) {
   massImp = numbConvert * 2.20462; // pega o valor do INPUT e transforma de Metrico -> Imperial
   massMet = numbConvert / 2.20462; // pega o valor do INPUT e transforma de Imperial -> Metrico
 
-  lenghtDiv.innerHTML = `<h3>Length (Meter/Feet)</h3> <p> ${Number(uniInput.value)} meters = ${lengthImp.toFixed(3)} feet | ${Number(uniInput.value)} feet = ${lengthMet.toFixed(3)} meters</p>`;
+  lengthDiv.innerHTML = `<h3>Length (Meter/Feet)</h3> <p> ${Number(uniInput.value)} meters = ${lengthImp.toFixed(3)} feet | ${Number(uniInput.value)} feet = ${lengthMet.toFixed(3)} meters</p>`;
 
   volumeDiv.innerHTML = `<h3>Volume (Liters/Gallons)</h3> <p> ${Number(uniInput.value)} liters = ${volumeImp.toFixed(3)} gallons | ${Number(uniInput.value)} gallons = ${volumeMet.toFixed(3)} liters</p>`;
 
   massDiv.innerHTML = `<h3>Mass (Kilograms/Pounds)</h3> <p> ${Number(uniInput.value)} kilos = ${massImp.toFixed(3)} pounds | ${Number(uniInput.value)} pounds = ${massMet.toFixed(3)} kilos</p>`;
+}
+
+cvtBtn.addEventListener("click", calcConversion);
+uniInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") calcConversion();
 });
-
-// function calcConversion (numbConvert) {
-//     uniInput.textContent = numbConvert
-//     lengthConvert = numbConvert * 3.281
-//     volumeConvert = numbConvert * 0.264
-//     massConvert = numbConvert * 2.204
-
-//     console.log(lengthConvert)
-//     console.log(volumeConvert)
-//     console.log(massConvert)
-// }
